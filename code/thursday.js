04d655484f69252f99fe3a987635ab3b35783b89
JOE CHURCHMAN <CHURCHMAN.JOE@GMAIL.COM>
2017-04-20 19:30:00 -0700

    Mid class update after lots of notes

--- code/thursday.js
+++ code/thursday.js
@@ -1,28 +1,32 @@
-var day = 'Thursday',
+var day = 'Monday',
     thursday = 'thursday';
 
-// How many equals signs does it take to screw your code?
-if (day = 'thursday') {
-  console.log('Test 1: Well lookee there, it\'s ' + day);
-}
-
-// The case of the failing "if" statement
-if (day == 'Thursday') {
-  console.log('Test 2: Lo and behold, it\'s ' + day);
-}
-
-// Equals thrice, test once
-if (day === 'Thursday') {
-  console.log('Test 3: Huh. It\'s ' + day);
-}
-
+// // How many equals signs does it take to screw your code?
+// if (day = 'thursday') {
+//   console.log('Test 1: Well lookee there, it\'s ' + day);
+// }
+//
+// // The case of the failing "if" statement
+// if (day == 'Thursday') {
+//   console.log('Test 2: Lo and behold, it\'s ' + day);
+// }
+//
+// // Equals thrice, test once
+// if (day === 'Thursday') {
+//   console.log('Test 3: Huh. It\'s ' + day);
+// }
+//
 // TODO: After commenting out the 3 conditionals above,
 //       uncomment this code and run it.
-// if (day === 'Thursday') {
-//   console.log('Is it Friday yet?');
-// } else {
-//   console.log('Whistle while you work!')
-// }
+if (day === 'Thursday') {
+  console.log('Is it Friday yet?');
+} else if (day === 'Saturday' || day === 'Sunday'){
+  console.log("Fuck yeah it's the weekend!")
+} else if (day === 'Monday'){
+  console.log('Monday Monkey lives for the weekend.')
+} else {
+  console.log('Whistle while you work!')
+}
 
 // TODO: Add an "else if" block to check whether it's Saturday
 //       and console.log() a different statement for the weekend.
@@ -53,8 +57,8 @@ var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 
 // console.log('index 7: ' + days[7]);
 // console.log('index 20: ' + days[20]);
 
-// TODO: DRY it up!
-// for (var i = 0; i < 7; i++) {
+// // TODO: DRY it up!
+// for (var i = 0; i < days.length; i++) {
 //   console.log('index ' + i + ': ' + days[i]);
 // }
 
@@ -65,8 +69,9 @@ var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 
 // TODO: On each iteration, use our randomly generated dayNumber
 //       to get a day from the days array, and console.log the
 //       name of the day.
-// var max = 100,
-//     result;
-// for (var j = 0; j < max; j++) {
-//   dayNumber = Math.floor(Math.random() * days.length);
-// }
+var max = 100,
+    result;
+for (var j = 0; j < max; j++) {
+  daysNumber = Math.floor(Math.random() * days.length);
+  console.log(days[daysNumber])
+}
--- notes/thursday-jchurchman.md
+++ notes/thursday-jchurchman.md
@@ -1,6 +1,34 @@
-git Status
-git checkout -b <FILENAME>
-create file
-git add <directory>\<FILENAME>
-git commit -m "<COMMIT MESSAGE>"
-git push origin <directory>
+git steps
+What's going on?                      git Status
+Create BRANCH                         git checkout -b <FILENAME>
+Create new file                       git touch <directory>\<filename>
+Stage new file for committing         git add <directory>\<FILENAME>
+Commit newly made file to local repo  git commit -m "<COMMIT MESSAGE>"
+push new file to remote repo          git push origin <directory>
+
+Vocab
+repository = collection of files
+remote = literally an alias for a URL that is the location of the copy of your repo
+branch = bookmark of changes in files of repository
+
+Shortcuts
+"ctrl + /" shortcut for "comment out" a block of code
+
+
+Javascript concepts:
+truthy - 1, 'thursday', 8, 28.65, 'false'
+falsey - 0, undefined, null, false,
+
+= defines a variable
+== can X equate Y? For Example: "2 == '2' yields a result of 'true'"
+=== tests perfect truthiness
+
+
+Advice:
+1. Make it run
+  then make it right
+  then make it fast (pretty)
+2. DRY
+  Don't
+  Repeat
+  Yourself
